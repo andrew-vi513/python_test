c51f167e8299fb768022a0f74e3a1b1f941fd2d6
andrew-vi513 <115106548+andrew-vi513@users.noreply.github.com>
2022-10-16 09:16:56 +0300

    bot

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-print("Hello world!")
-x = 5
-print(x)
-x_2 = 5
+what = input('Что делаем? (+, -): ')
+
+a = float(input('Введи первое число: '))
+b = float(input('Введи второе число: '))
+
+if what == '+':
+    c = a + b
+    print('Результат: ' + str(c))
+elif what == '-':
+    c = a - b
+    print('Результат: ' + str(c))
+else:
+    print('Выбрана неверная операция!')
+
